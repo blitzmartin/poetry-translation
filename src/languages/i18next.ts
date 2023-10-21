d6427a0b957adc000039023ae10f5c022c268288
blitzmartin <lizmartinmusic@gmail.com>
2023-10-21 14:12:59 +0200

    add es fr de

--- src/languages/i18next.ts
+++ src/languages/i18next.ts
@@ -1,9 +1,17 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import de from './de.json'
 import en from './en.json'
+import es from './es.json'
+import fr from './fr.json'
+import it from './it.json'
 
 export const resources = {
-  en
+  en,
+  it,
+  es,
+  fr,
+  de
 } as const
 
 i18n.use(initReactI18next).init({

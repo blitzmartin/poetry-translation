7319efc25a081c2d3bcff8edcf1227432710f3e9
blitzmartin <lizmartinmusic@gmail.com>
2023-10-21 15:40:58 +0200

    style not found

--- src/shared/NotFound.tsx
+++ src/shared/NotFound.tsx
@@ -1,3 +1,7 @@
 export const NotFound = () => {
-  return <div>NOT FOUND</div>
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div className="text-8xl font-extrabold">NOT FOUND</div>
+    </div>
+  )
 }
